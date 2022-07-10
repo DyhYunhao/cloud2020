6346b35571779564ead4ec835ed3f54a1ee13ddc
daiyunhao <2609159632@qq.com>
2022-07-10 10:38:56 +0800

    eureka单机

--- cloud-eureka-server7001/src/main/java/com/dyh/springcloud/EurekaServerMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/dyh/springcloud/EurekaServerMain7001.java
@@ -2,17 +2,17 @@ package com.dyh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @auther daiyunhao
- * @create 2022/7/7
+ * @create 2022/7/9
  * @desc
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaServerMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaServerMain7001.class, args);
     }
 }
