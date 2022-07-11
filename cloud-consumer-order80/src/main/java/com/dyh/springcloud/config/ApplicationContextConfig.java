eaac74234bf88c0b5f319dca56846e029e3801c2
daiyunhao <2609159632@qq.com>
2022-07-11 15:18:34 +0800

    eureka集群

--- cloud-consumer-order80/src/main/java/com/dyh/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/dyh/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.dyh.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 /**
  * @auther daiyunhao
  * @create 2022/7/7
- * @desc  当前文件就是spring的配置文件
+ * @desc 当前文件就是spring的配置文件
  */
 @Configuration
 public class ApplicationContextConfig {
@@ -16,7 +17,7 @@ public class ApplicationContextConfig {
      * 配置bean 不然后面没法依赖注入，这里bean就像以前ssm整合时配置依赖注入一样
      */
     @Bean
-    //    @LoadBalanced //让这个RestTemplate在请求时拥有客户端负载均衡的能力  //将此注解注释掉，使用自己的轮询算法不使用默认的
+    @LoadBalanced //让这个RestTemplate在请求时拥有客户端负载均衡的能力  //将此注解注释掉，使用自己的轮询算法不使用默认的
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
