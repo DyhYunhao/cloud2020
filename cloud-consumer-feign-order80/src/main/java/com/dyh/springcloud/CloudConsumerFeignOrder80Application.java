3de780ec08f88284d122f604bdf5582a4f91243c
daiyunhao <2609159632@qq.com>
2022-07-23 20:04:50 +0800

    openFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/dyh/springcloud/CloudConsumerFeignOrder80Application.java
@@ -0,0 +1,21 @@
+package com.dyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @auther daiyunhao
+ * @create 2022/7/23
+ * @desc
+ */
+@SpringBootApplication
+@EnableFeignClients // 启动feign
+public class CloudConsumerFeignOrder80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerFeignOrder80Application.class, args);
+
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/dyh/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,44 @@
+package com.dyh.springcloud.controller;
+
+import ch.qos.logback.core.util.TimeUtil;
+import com.dyh.springcloud.entities.CommonResult;
+import com.dyh.springcloud.entities.Payment;
+import com.dyh.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @auther daiyunhao
+ * @create 2022/7/23
+ * @desc
+ */
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        //故意超时设置
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // OpenFeign客户端一般默认等待1秒钟
+        return paymentFeignService.paymentFeignTimeout();
+//        return null;
+    }
+}
