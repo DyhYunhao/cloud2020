0343c7cbdb515453ce8bb08043fd0da49829aab7
daiyunhao <2609159632@qq.com>
2022-08-18 10:16:06 +0800

    Hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/dyh/springcloud/CloudConsumerFeignHystrixOrder80Application.java
@@ -0,0 +1,20 @@
+package com.dyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @auther daiyunhao
+ * @create 2022/8/2
+ * @desc
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class CloudConsumerFeignHystrixOrder80Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerFeignHystrixOrder80Application.class, args);
+    }
+}
