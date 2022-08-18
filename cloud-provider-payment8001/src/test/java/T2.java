236c4c9a61baf719166f9f76afadd86b8bdf897d
daiyunhao <2609159632@qq.com>
2022-08-18 17:04:43 +0800

    Gateway

---
+++ cloud-provider-payment8001/src/test/java/T2.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj); //2022-08-18T16:40:01.306+08:00[Asia/Shanghai]
+    }
+}
