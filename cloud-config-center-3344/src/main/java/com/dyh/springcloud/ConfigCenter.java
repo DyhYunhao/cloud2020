7150535fdec23e277bebc540093e3178b073070b
daiyunhao <2609159632@qq.com>
2022-08-22 11:18:36 +0800

    config

---
+++ cloud-config-center-3344/src/main/java/com/dyh/springcloud/ConfigCenter.java
@@ -0,0 +1,17 @@
+package com.dyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @auther daiyunhao
+ * @create 2022/8/19
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenter {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter.class, args);
+    }
+}
